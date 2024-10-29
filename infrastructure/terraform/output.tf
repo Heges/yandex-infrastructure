31279b0d65b326818448cd1161c2eb237344e929
gaag_alexander <ikunkka92@gmail.com>
2024-10-30 00:32:48 +0300

    start pipiline

--- infrastructure/terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "Идентификатор созданной виртуальной машины"
-  value       = module.tf-yc-instance.instance_id
-}
-
-output "instance_ip" {
-  description = "Внешний IP-адрес виртуальной машины"
-  value       = module.tf-yc-instance.instance_ip
-}
